6f925231abcb7f5b55985dd9e74b6a9b721c03ea
Developer <developer@Developers-iMac-2.local>
2020-11-06 11:26:56 +0200

    feature: create project depend filter [ref:29]

--- lib/screens/projects/projects.screen.dart
+++ lib/screens/projects/projects.screen.dart
@@ -94,63 +94,69 @@ class _ProjectsScreenState extends State<ProjectsScreen> {
             body: ListView(
               children: <Widget>[
                 state.filter != null
-                    ? Wrap(children: <Widget>[
-                        state.filter?.user?.id != null
-                            ? InkWell(
-                                onTap: () {
-                                  store.dispatch(SaveProjectsFilter(state.filter
-                                      .copyWith(user: UserModel())));
-                                },
-                                child: FilterItemWidget(
-                                  title:
-                                      '${state.filter.user.name} ${state.filter.user.lastName}',
-                                  icon: Icons.person,
-                                ),
-                              )
-                            : Container(),
-                        state.filter?.stack?.id != null
-                            ? InkWell(
-                                onTap: () {
-                                  store.dispatch(SaveProjectsFilter(state.filter
-                                      .copyWith(stack: StackModel())));
-                                },
-                                child: FilterItemWidget(
-                                  title: state.filter.stack.name,
-                                  icon: Icons.menu,
-                                ),
-                              )
-                            : Container(),
-                        state.filter?.spec?.title != null &&
-                                state.filter.spec.spec != ProjectSpec.All
-                            ? InkWell(
-                                onTap: () {
-                                  store.dispatch(SaveProjectsFilter(state.filter
-                                      .copyWith(
-                                          spec: ProjectSpecModel(
-                                              'All', ProjectSpec.All))));
-                                },
-                                child: FilterItemWidget(
-                                  title: state.filter.spec?.title,
-                                  icon: Icons.menu,
-                                ),
-                              )
-                            : Container(),
-                        state.filter?.status?.title != null &&
-                                state.filter.status.status != ProjectStatus.All
-                            ? InkWell(
-                                onTap: () {
-                                  store.dispatch(SaveProjectsFilter(state.filter
-                                      .copyWith(
-                                          status: ProjectStatusModel(
-                                              'All', ProjectStatus.All))));
-                                },
-                                child: FilterItemWidget(
-                                  title: state.filter.status?.title,
-                                  icon: Icons.menu,
-                                ),
-                              )
-                            : Container(),
-                      ])
+                    ? Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Wrap(children: <Widget>[
+                          state.filter?.user?.id != null
+                              ? InkWell(
+                                  onTap: () {
+                                    store.dispatch(SaveProjectsFilter(state
+                                        .filter
+                                        .copyWith(user: UserModel())));
+                                  },
+                                  child: FilterItemWidget(
+                                    title:
+                                        '${state.filter.user.name} ${state.filter.user.lastName}',
+                                    icon: Icons.person,
+                                  ),
+                                )
+                              : Container(),
+                          state.filter?.stack?.id != null
+                              ? InkWell(
+                                  onTap: () {
+                                    store.dispatch(SaveProjectsFilter(state
+                                        .filter
+                                        .copyWith(stack: StackModel())));
+                                  },
+                                  child: FilterItemWidget(
+                                    title: state.filter.stack.name,
+                                    icon: Icons.menu,
+                                  ),
+                                )
+                              : Container(),
+                          state.filter?.spec?.title != null &&
+                                  state.filter.spec.spec != ProjectSpec.All
+                              ? InkWell(
+                                  onTap: () {
+                                    store.dispatch(SaveProjectsFilter(
+                                        state.filter.copyWith(
+                                            spec: ProjectSpecModel(
+                                                'All', ProjectSpec.All))));
+                                  },
+                                  child: FilterItemWidget(
+                                    title: state.filter.spec?.title,
+                                    icon: Icons.menu,
+                                  ),
+                                )
+                              : Container(),
+                          state.filter?.status?.title != null &&
+                                  state.filter.status.status !=
+                                      ProjectStatus.All
+                              ? InkWell(
+                                  onTap: () {
+                                    store.dispatch(SaveProjectsFilter(
+                                        state.filter.copyWith(
+                                            status: ProjectStatusModel(
+                                                'All', ProjectStatus.All))));
+                                  },
+                                  child: FilterItemWidget(
+                                    title: state.filter.status?.title,
+                                    icon: Icons.menu,
+                                  ),
+                                )
+                              : Container(),
+                        ]),
+                      )
                     : Container(),
                 _projects(state)
               ],
@@ -162,114 +168,125 @@ class _ProjectsScreenState extends State<ProjectsScreen> {
   Widget _projects(_ViewModel state) {
     return state.isLoading
         ? Container()
-        : ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: state.projects.length,
-            itemBuilder: (BuildContext context, int index) {
-              final ProjectModel project = state.projects[index];
-              return Opacity(
-                opacity:
-                    project.isInternal || project.endDate != null ? 0.6 : 1,
-                child: Slidable(
-                  enabled: state.user.position == Positions.OWNER &&
-                      project.endDate == null,
-                  controller: _slidableController,
-                  actionPane: const SlidableDrawerActionPane(),
-                  actionExtentRatio: 0.1,
-                  secondaryActions: <Widget>[
-                    IconSlideAction(
-                      color: AppColors.bg,
-                      iconWidget: IconButton(
-                          icon: const Icon(Icons.archive),
-                          color: Colors.white,
-                          onPressed: () async {
-                            final bool isConfirm = await showDialog(
-                                barrierDismissible: false,
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return const ConfirmDialogWidget(
-                                      title: 'Project',
-                                      text:
-                                          'Are you sure to finish the project?');
-                                });
-                            store.dispatch(SetTitle('Projects'));
-                            if (!isConfirm) {
-                              return;
-                            }
-                            store.dispatch(
-                                ArchiveProjectPending(project.id, 'finished'));
-                          }),
-                    ),
-                    IconSlideAction(
-                      color: AppColors.bg,
-                      iconWidget: IconButton(
-                          icon: const Icon(Icons.cancel),
-                          color: Colors.white,
-                          onPressed: () async {
-                            final bool isConfirm = await showDialog(
-                                barrierDismissible: false,
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return const ConfirmDialogWidget(
-                                      title: 'Project',
-                                      text:
-                                          'Are you sure to reject the project?');
-                                });
-                            store.dispatch(SetTitle('Projects'));
-                            if (!isConfirm) {
-                              return;
-                            }
-                            store.dispatch(
-                                ArchiveProjectPending(project.id, 'rejected'));
-                          }),
-                    ),
-                  ],
-                  child: AppListTile(
-                    onTap: () {
-                      store.dispatch(SetTitle(project.name));
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute<void>(
-                              builder: (BuildContext context) =>
-                                  ProjectDetailsScreen(
-                                    projectId: project.id,
-                                  )));
-                    },
-                    leading: Container(
-                      width: MediaQuery.of(context).size.width / 5,
-                      child: Text(project.name,
-                          style: const TextStyle(
-                              fontSize: 16, color: Colors.white)),
-                    ),
-                    title: Wrap(
-                        children: project.stack
-                            .map(
-                              (StackModel stack) => Container(
-                                padding: const EdgeInsets.all(4),
-                                margin: const EdgeInsets.symmetric(
-                                    horizontal: 4, vertical: 4),
-                                decoration: BoxDecoration(
-                                    color: AppColors.red,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Text(stack.name,
-                                    style: const TextStyle(
-                                      color: Colors.white,
-                                    )),
-                              ),
-                            )
-                            .toList()),
-                    trailing: Container(
-                      width: MediaQuery.of(context).size.width / 5.2,
-                      child: Text(
-                        '${converter.dateFromString((project.startDate).toString())} - ${project.endDate != null ? converter.dateFromString((project.endDate).toString()) : 'now'}',
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
+        : state.projects.isEmpty
+            ? const Padding(
+                padding: EdgeInsets.only(bottom: 16),
+                child: Center(
+                  child: Text(
+                    'No projects',
+                    style: TextStyle(fontSize: 18, color: AppColors.red),
                   ),
                 ),
-              );
-            });
+              )
+            : ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: state.projects.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ProjectModel project = state.projects[index];
+                  return Opacity(
+                    opacity:
+                        project.isInternal || project.endDate != null ? 0.6 : 1,
+                    child: Slidable(
+                      enabled: state.user.position == Positions.OWNER &&
+                          project.endDate == null,
+                      controller: _slidableController,
+                      actionPane: const SlidableDrawerActionPane(),
+                      actionExtentRatio: 0.1,
+                      secondaryActions: <Widget>[
+                        IconSlideAction(
+                          color: AppColors.bg,
+                          iconWidget: IconButton(
+                              icon: const Icon(Icons.archive),
+                              color: Colors.white,
+                              onPressed: () async {
+                                final bool isConfirm = await showDialog(
+                                    barrierDismissible: false,
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return const ConfirmDialogWidget(
+                                          title: 'Project',
+                                          text:
+                                              'Are you sure to finish the project?');
+                                    });
+                                store.dispatch(SetTitle('Projects'));
+                                if (!isConfirm) {
+                                  return;
+                                }
+                                store.dispatch(ArchiveProjectPending(
+                                    project.id, 'finished'));
+                              }),
+                        ),
+                        IconSlideAction(
+                          color: AppColors.bg,
+                          iconWidget: IconButton(
+                              icon: const Icon(Icons.cancel),
+                              color: Colors.white,
+                              onPressed: () async {
+                                final bool isConfirm = await showDialog(
+                                    barrierDismissible: false,
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return const ConfirmDialogWidget(
+                                          title: 'Project',
+                                          text:
+                                              'Are you sure to reject the project?');
+                                    });
+                                store.dispatch(SetTitle('Projects'));
+                                if (!isConfirm) {
+                                  return;
+                                }
+                                store.dispatch(ArchiveProjectPending(
+                                    project.id, 'rejected'));
+                              }),
+                        ),
+                      ],
+                      child: AppListTile(
+                        onTap: () {
+                          store.dispatch(SetTitle(project.name));
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute<void>(
+                                  builder: (BuildContext context) =>
+                                      ProjectDetailsScreen(
+                                        projectId: project.id,
+                                      )));
+                        },
+                        leading: Container(
+                          width: MediaQuery.of(context).size.width / 5,
+                          child: Text(project.name,
+                              style: const TextStyle(
+                                  fontSize: 16, color: Colors.white)),
+                        ),
+                        title: Wrap(
+                            children: project.stack
+                                .map(
+                                  (StackModel stack) => Container(
+                                    padding: const EdgeInsets.all(4),
+                                    margin: const EdgeInsets.symmetric(
+                                        horizontal: 4, vertical: 4),
+                                    decoration: BoxDecoration(
+                                        color: AppColors.red,
+                                        borderRadius:
+                                            BorderRadius.circular(10)),
+                                    child: Text(stack.name,
+                                        style: const TextStyle(
+                                          color: Colors.white,
+                                        )),
+                                  ),
+                                )
+                                .toList()),
+                        trailing: Container(
+                          width: MediaQuery.of(context).size.width / 5.2,
+                          child: Text(
+                            '${converter.dateFromString((project.startDate).toString())} - ${project.endDate != null ? converter.dateFromString((project.endDate).toString()) : 'now'}',
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                });
   }
 
   SpeedDialChild speedDialChild(Function func, Widget icon) {
