24480087e303bc5c065f9ee8461de61fe015575c
Developer <developer@Developers-iMac-2.local>
2020-11-16 15:07:41 +0200

    feature: creating appscreen service [ref: no-ref]

--- lib/common/helpers/app-screen.dart
+++ lib/common/helpers/app-screen.dart
@@ -1,14 +1,6 @@
 import 'package:flutter/widgets.dart';
 
 class AppScreen {
-  static double getWidth(BuildContext context) {
-    return MediaQuery.of(context).size.width;
-  }
-
-  static double getHeight(BuildContext context) {
-    return MediaQuery.of(context).size.height;
-  }
-
   static double getRatio(BuildContext context) {
     return MediaQuery.of(context).size.width /
         MediaQuery.of(context).size.height;
